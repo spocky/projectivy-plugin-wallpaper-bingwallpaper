612c22842769e610ef357aa7d089a8f1d9785b41
spocky <spocky12+dev@gmail.com>
2025-09-02 22:58:04 +0200

    Added check for Projectivy Launcher on the device

--- bingwallpaper/src/main/java/tv/projectivy/plugin/wallpaperprovider/bingwallpaper/SettingsActivity.kt
+++ bingwallpaper/src/main/java/tv/projectivy/plugin/wallpaperprovider/bingwallpaper/SettingsActivity.kt
@@ -1,26 +1,32 @@
 package tv.projectivy.plugin.wallpaperprovider.bingwallpaper
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.GuidedStepSupportFragment
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import tv.projectivy.plugin.wallpaperprovider.api.Wallpaper
-import tv.projectivy.plugin.wallpaperprovider.api.WallpaperDisplayMode
-import tv.projectivy.plugin.wallpaperprovider.api.WallpaperType
 
 class SettingsActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (!packageManager.isApplicationInstalled("com.spocky.projengmenu")) {
+            Toast.makeText(this, R.string.projectivy_not_installed, Toast.LENGTH_LONG).show()
+        }
+
         val fragment: GuidedStepSupportFragment = SettingsFragment()
         if (savedInstanceState == null) {
             GuidedStepSupportFragment.addAsRoot(this, fragment, android.R.id.content)
+        }
+    }
+
+    fun PackageManager.isApplicationInstalled(packageName: String): Boolean {
+        return try {
+            getApplicationInfo(packageName, 0)
+            true
+        } catch (_: PackageManager.NameNotFoundException) {
+            false
         }
     }
 }
